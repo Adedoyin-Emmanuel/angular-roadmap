44abdba396b683893fa51c780b5ab3f2eb81b167
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-28 04:54:20 +0100

    Finished the star component challenge

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.css']
+  styleUrls: ['./star-rating.component.css'],
 })
 export class StarRatingComponent {
+  isClicked = true;
 
+  handleClick() {
+    this.isClicked = !this.isClicked;
+  }
 }
