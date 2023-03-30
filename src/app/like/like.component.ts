977dc3806985e698fd79ba4003a3d33c2cb0b889
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-30 13:07:15 +0100

    Finished the twitter like component clone challenge

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface likeComponentProp
+{
+    likeCount:number;
+    isActive:boolean;
+}
 @Component({
   selector: 'app-like',
   templateUrl: './like.component.html',
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent {
+  likeComponent: likeComponentProp = {
+    likeCount: 0,
+    isActive: false
+  }
 
+  handleClick()
+  {
+    this.likeComponent.isActive = !this.likeComponent.isActive;
+    if(this.likeComponent.isActive)
+    {
+      this.likeComponent.likeCount++;
+    }else{
+      this.likeComponent.likeCount--;
+    }
+  }
 }
