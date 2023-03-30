fa30eb5fed13d3f5a0be82dd834d7bcf0dd6548f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-30 09:06:48 +0100

    Solved the custom pipe challenge

--- src/app/input/input.pipe.ts
+++ src/app/input/input.pipe.ts
@@ -2,37 +2,30 @@ import { TitleCasePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'input'
+  name: 'input',
 })
 export class InputPipe implements PipeTransform {
-
   transform(value: string): any {
+    if (!value) return null;
 
-    if(!value)
-        return null;
+    const prep = ['of', 'the'];
+    //split the string by empty spaces
+    const splitText = value.split(' ');
 
-    const prep = [
-        "of",
-        "the"
-    ];
-
-    const splitText = value.split(/\s+/);
-
-    for(let i = 0; i < prep.length; i++)
-    {
-      for (let j = 0; j < splitText.length; j++) {
-          if(splitText[j].includes(prep[i]) && j != 0)
-          {
-          //we capitalize each word
-            console.log();
-          }
+    for (let i = 0; i < splitText.length; i++) {
+      if (prep.includes(splitText[i].toLowerCase()) && i !== 0) {
+        splitText[i] = splitText[i].toLowerCase();
+      } else {
+        //capitalize the words
+        splitText[i] =
+          splitText[i].substring(0, 1).toUpperCase() +
+          splitText[i].substring(1).toLowerCase();
       }
-
     }
-
 
     //console.log(value.split(/\s+/));
     //return value.split(/\s+/);
-  }
 
+    return splitText.join(' ');
+  }
 }
